03fe3154cad2a96339b4f6c7469f51509855e4ff
kirjava113 <annaluizalyjak@gmail.com>
2018-12-15 20:41:44 +0100

    Database script for adding Wind_Direction records

---
+++ database/WindDirection.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('1', 'N');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('2', 'NNE');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('3', 'NE');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('4', 'ENE');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('5', 'E');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('6', 'ESE');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('7', 'SE');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('8', 'SSE');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('9', 'S');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('10', 'SSW');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('11', 'SW');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('12', 'WSW');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('13', 'W');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('14', 'WNW');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('15', 'NW');
+INSERT INTO `Wind_Direction` (`Id`, `Direction`) VALUES ('16', 'NNW');
